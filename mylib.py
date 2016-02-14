9b96774adcacb76573ce0023831bfbfecd748729
Urmat Bekbo <bek.urmat@gmail.com>
2016-02-15 00:26:13 +0600

    My Functions Library - Module

---
+++ mylib.py
@@ -0,0 +1,13 @@
+# coding: utf8
+def get_integer(text):
+    """Function returns integer until it is True"""
+    
+    y = False
+    while not y:
+        try:
+            input_value = int(raw_input(text))
+        except ValueError:
+            print 'Это не цифра !'
+        else:
+            y = True
+            return input_value
