de71554970d347f1baa512bdef101f93f15512ff
Urmat Bekbo <bek.urmat@gmail.com>
2016-02-15 15:59:07 +0600

    Optimized homework_2

--- homework_2.py
+++ homework_2.py
@@ -1,32 +1,22 @@
 # coding: utf8
-
-# Function get_integer - returns integer or error
-def get_integer(text):
-    try:
-         input_value = int(raw_input(text))
-    except ValueError:
-        return 'error'
-    else:
-        return input_value
+import mylib     # My functions
 
 # HomeWork 1.2
 print u'Общество в начале XXI века\n'
-x = get_integer('Введите ваш возраст: ')
-if (x == 'error' or x <= 0 or x > 120):
-    print 'Ошибка, это программа для людей'
-else:
-    # Option 1
-    if x <= 6:
-        print 'Вам в детский сад'
-    # Option 2
-    elif x <= 17:
-        print 'Вам в школу'
-    # Option 3
-    elif x <= 24:
-        print 'Вам в профессиональное учебное заведение'
-    # Option 4
-    elif x <= 59:
-        print 'Вам на работу'
-    # Option 5
-    elif x <= 120:
-        print 'Вам предоставляется выбор'
+
+list = ('Ошибка это программа для людей', 'Вам в детский сад !', 'Вам в школу',
+        'Вам в профессиональное учебное заведение', 'Вам на работу', 'Вам предоставляется выбор')
+
+x = mylib.get_integer('Введите ваш возраст: ')
+if (x <= 0 or x > 120):
+    print list[0]
+elif x <= 6:    # Option 1
+    print list[1]
+elif x <= 17:   # Option 2
+    print list[2]
+elif x <= 24:   # Option 3
+    print list[3]
+elif x <= 59:   # Option 4
+    print list[4]
+elif x <= 120:  # Option 5
+    print list[5]
