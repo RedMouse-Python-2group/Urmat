120a62e7584c49e0e674634e2bb69cc7302cdda9
Urmat Bekbo <bek.urmat@gmail.com>
2016-02-26 02:26:20 +0600

    Homework 6.2 - Done

--- homework_2.py
+++ homework_2.py
@@ -1,12 +1,30 @@
 # coding: utf8
 
-class Task2():  # HomeWork 1.2
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class Unknown():
+    __metaclass__ = ABCMeta
+    x = 0
+
+class Task2(Unknown):  # HomeWork 1.2
     list = ('Ошибка это программа для людей', 'Вам в детский сад !', 'Вам в школу',
             'Вам в профессиональное учебное заведение', 'Вам на работу', 'Вам предоставляется выбор')
 
     def __init__(self):
         print u'Общество в начале XXI века\n'
         self.x = self.get_integer('Введите ваш возраст: ')
+        if (self.x <= 0 or self.x > 120):
+            print self.list[0]
+        elif self.x <= 6:    # Option 1
+            print self.list[1]
+        elif self.x <= 17:   # Option 2
+            print self.list[2]
+        elif self.x <= 24:   # Option 3
+            print b.list[3]
+        elif self.x <= 59:   # Option 4
+            print self.list[4]
+        elif self.x <= 120:  # Option 5
+            print self.list[5]
 
     def get_integer(self, text):
         """Function returns integer until it is True"""
@@ -20,16 +38,8 @@ class Task2():  # HomeWork 1.2
                 y = True
                 return int(input_value)
 
-b = Task2()
-if (b.x <= 0 or b.x > 120):
-    print b.list[0]
-elif b.x <= 6:    # Option 1
-    print b.list[1]
-elif b.x <= 17:   # Option 2
-    print b.list[2]
-elif b.x <= 24:   # Option 3
-    print b.list[3]
-elif b.x <= 59:   # Option 4
-    print b.list[4]
-elif b.x <= 120:  # Option 5
-    print b.list[5]
+class Child(Task2):
+    def __init__(self):
+        super(Child, self).__init__()
+
+b = Child()
