a7004504757ac846c74e7447575c2f292d627f96
Urmat Bekbo <bek.urmat@gmail.com>
2016-03-28 01:12:15 +0600

    Created model Article, Created Database

--- myblog/article/models.py
+++ myblog/article/models.py
@@ -5,8 +5,16 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     class Meta():
-        db_table = "article"
-    article_title = models.CharField(max_length = 200)
+        db_table = 'article'
+        
+    article_title = models.CharField(max_length=200)
     article_text = models.TextField()
     article_date = models.DateTimeField()
-    article_likes = models.IntegerField()
+    article_likes = models.IntegerField(default=0)    # or we can use blank=true, null=true
+
+class Comments(models.Model):
+    class Meta():
+        db_table = 'comments'
+
+    comments_text = models.TextField()
+    comments_article = models.ForeignKey(Article)
